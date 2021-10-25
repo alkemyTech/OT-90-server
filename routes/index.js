36d0f961a769541ad40f1faceef78a1868dadc49
Dario Velez Roman <dvelezroman@gmail.com>
2021-10-24 22:48:49 -0300

    configure eslint prettier and precommit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var router = express.Router();
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const express = require('express')
 
-module.exports = router;
+const router = express.Router()
+/* GET home page. */
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Express' })
+})
+
+module.exports = router
