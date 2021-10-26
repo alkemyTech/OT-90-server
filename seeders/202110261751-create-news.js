3a4076f70cc0954a30c59ded3cf8d247befff8e4
jluduena <jluduena@flexiti.com>
2021-10-26 18:34:48 -0300

    news seeders

---
+++ seeders/202110261751-create-news.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('News', [{
+      name: 'nameuno',
+      content: 'contenidouno',
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      categoryId: 1,
+      type: 'event',
+      deletedAt: new Date,
+    },
+    {
+      name: 'namedos',
+      content: 'contenidodos',
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      categoryId: 2,
+      type: 'news',
+      deletedAt: new Date,
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
