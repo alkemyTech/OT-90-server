a144a3e237191b03ae77b62120cd5a2aae47e06f
jluduena <jluduena@flexiti.com>
2021-10-26 10:16:18 -0300

    model news

---
+++ models/news.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Entries extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+     //categoryId.belongsTo(models.Category, {as: 'id'});
+    }
+  };
+  Entries.init({
+    name: DataTypes.STRING,
+    content: DataTypes.STRING,
+    image: DataTypes.STRING,
+    categoryId: DataTypes.INTEGER,
+    type : DataTypes.STRING,
+    deletedAt : DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Entries',
+  });
+  return Entries;
+};
