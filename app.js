887ce68c6b4aa19b298d4ad43ae5f7991e1f8d9e
Maximiliano Paci <maximiliano.paci@sarsy-srl.com>
2021-10-27 08:45:52 -0300

    renaming categoriesRoute to categoriesNetwork in app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
-const categoriesRouter = require('./components/categories/network')
+const categoriesNetwork = require('./components/categories/network')
 
 const app = express()
 app.use(cors())
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-app.use('/categories', categoriesRouter)
+app.use('/categories', categoriesNetwork)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
