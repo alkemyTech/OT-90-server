3a0618fa2bd5c88cb9621c63a97a59d44002655b
jluduena <jluduena@flexiti.com>
2021-10-27 17:01:58 -0300

    change routes from app.js routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 
-const router = express.Router()
-/* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' })
-})
+const router = express.Router();
+
+const usersNetwork = require('../components/users/network')
+
+router.use('/users', usersNetwork)
 
 module.exports = router
