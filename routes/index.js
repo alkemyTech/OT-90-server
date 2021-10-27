5fb804c92db3e10deb644f5d8e2f441d71b5764b
verteramo.federico <verteramo.federico@gmail.com>
2021-10-27 14:39:05 -0300

    comments deleted

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,11 @@ const express = require('express')
 const router = express.Router()
 
 const news = require('../components/news/network')
-/* const newsAuthMiddleware = require('../middlewares/newsAuthMiddleware') */
 
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express' })
 })
 
-router.use('/news', /* newsAuthMiddleware, */ news)
+router.use('/news', news)
 
 module.exports = router
