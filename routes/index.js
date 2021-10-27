273fca3ce1ba5390beead223c2af8048e50421cf
Marcelo Nieva <marcelonieva@gmail.com>
2021-10-27 16:58:49 -0300

    create members route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
 const router = express.Router()
+const membersNetwork = require('../components/members/network')
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express' })
 })
+router.use('/members', membersNetwork)
 
 module.exports = router
