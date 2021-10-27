e863683d334b40d6eaac902a87133b31cc9b2b4b
verteramo.federico <verteramo.federico@gmail.com>
2021-10-27 11:02:08 -0300

    bug fixed in middleware

--- middlewares/newsAuthMiddleware.js
+++ middlewares/newsAuthMiddleware.js
@@ -1,11 +1,12 @@
-import { User } from '../models/index'
+const db = require('../models');
 
-function newsAuthMiddleware (req, res, next) {
-	const userAdmin = User.findOne({ where: { roleId: 0 } })
+// eslint-disable-next-line consistent-return
+const newsAuthMiddleware = (req, res, next) => {
+  const userAdmin = db.User.findOne({ where: { roleId: 0 } })
   if (!userAdmin) {
     return res.status(401).send({ Error: 'Auth failed' })
   }
   next()
 }
 
-export default newsAuthMiddleware
+module.exports = newsAuthMiddleware
