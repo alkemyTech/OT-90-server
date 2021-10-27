13d7718a827dabc1842a52ae6e2f9ca1b3c44267
verteramo.federico <verteramo.federico@gmail.com>
2021-10-27 09:44:08 -0300

    ticket basic structure ok

---
+++ components/news/network.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const controller = require('./controller')
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+  try {
+    const news = await controller.getAll()
+    res.status(200).send(news)
+  } catch (Error) {
+    res.status(500).send({ Error: 'Something has gone wrong' })
+  }
+})
+
+module.exports = router
