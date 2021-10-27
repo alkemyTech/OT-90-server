87af874c5ca2b1dbcf1baad266c68ac91e1d090d
maxipaci <62407328+maxipaci@users.noreply.github.com>
2021-10-27 18:42:12 -0300

    Ot90 712 (#12)
    * adding initial categories routes
    
    * adding category controller with its respective method 'getAll' to get all the categories
    
    * modifying category config model, setting timestamps to false
    
    * adding category store for database queries related to categories
    
    * adding category id in endpoint response
    
    * renaming categoriesRoute to categoriesNetwork in app.js
    
    * adding categories network to index
    
    Co-authored-by: Maximiliano Paci <pacimaximiliano@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const express = require('express')
 const router = express.Router()
 
 const usersNetwork = require('../components/users/network')
+const categoriesNetwork = require('../components/categories/network')
 
 router.use('/users', usersNetwork)
+router.use('/categories', categoriesNetwork)
 
 module.exports = router
