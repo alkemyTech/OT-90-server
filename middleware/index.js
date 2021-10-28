86e0f0bbbd957381b118757816f335dfb956e7ee
andresmonsa <monsalbefotografia@gmail.com>
2021-10-28 15:38:03 -0300

    handling error

--- middleware/index.js
+++ middleware/index.js
@@ -3,11 +3,15 @@ const { Role } = require('../models')
 module.exports = {
   isAdmin: async (req, res, next) => {
     const roleId = req.header('roleId')
-    const role = await Role.findByPk(roleId)
-    if (role.name === 'Admin') {
-      next()
-    } else {
-      res.status(403).send('Your user role have not authorization to make this request')
+    try {
+      const role = await Role.findByPk(roleId)
+      if (role.name === 'Admin') {
+        next()
+      } else {
+        res.status(403).send('Your user role have not authorization to make this request')
+      }
+    } catch (e) {
+      res.status(500).send('Somethins gone wrong')
     }
   }
 }
