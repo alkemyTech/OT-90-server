1448daa9b628206e9b666d9f84073dc229ec3163
AndresMonsa <monsalbefotografia@gmail.com>
2021-10-28 08:25:44 -0300

    Fixing slints bugs

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,11 @@ const express = require('express')
 
 const router = express.Router()
 
-
 const contactsNetwork = require('../components/contacts/network')
 const membersNetwork = require('../components/members/network')
 const usersNetwork = require('../components/user/network')
 const categoriesNetwork = require('../components/categories/network')
 const news = require('../components/news/network')
-
 
 router.use('/contacts', contactsNetwork)
 router.use('/members', membersNetwork)
