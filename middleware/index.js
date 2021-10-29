d2276e5832817c436b40205df9ac6a52be417142
AndresMonsa <monsalbefotografia@gmail.com>
2021-10-29 18:46:57 -0300

    OT90-738
    validation middleware (#25)

--- middleware/index.js
+++ middleware/index.js
@@ -13,5 +13,20 @@ module.exports = {
     } catch (e) {
       res.status(500).send('Somethins gone wrong')
     }
+  },
+  validation: (schema) => async (req, res, next) => {
+    try {
+      await schema.validate(
+        {
+          body: req.body,
+          query: req.query,
+          params: req.params
+        },
+        { abortEarly: false }
+      ) // abortEarly: false so it will show the entire list of errors
+      return next()
+    } catch (err) {
+      return res.status(400).json({ type: err.name, message: err.message, errors: err.errors })
+    }
   }
 }
