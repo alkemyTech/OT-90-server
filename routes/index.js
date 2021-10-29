4d331f806d96d6dd7c7f30e5f0a28b526897bbe6
FranMerchan <merchanf6@gmail.com>
2021-10-29 09:25:31 -0300

    routes/index.js fixed

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,6 @@ const categoriesNetwork = require('../components/categories/network')
 const news = require('../components/news/network')
 
 router.use('/contacts', contactsNetwork)
-router.use('/members', membersNetwork)
 router.use('/users', usersNetwork)
 router.use('/categories', categoriesNetwork)
 router.use('/news', news)
