7bf037b8d5c5f40979309398b604823a9723e210
jpuery71292 <jpuery@gmail.com>
2021-11-01 14:58:15 -0300

    Create Endpoint - new contact

--- components/contacts/network.js
+++ components/contacts/network.js
@@ -1,4 +1,7 @@
 const express = require('express')
+
+const { body, validationResult } = require('express-validator')
+
 const controller = require('./controller')
 
 const router = express.Router()
@@ -11,5 +14,23 @@ router.get('/', async (req, res) => {
     res.status(500).send({ Error: 'Something has gone wrong' })
   }
 })
+
+router.post('/',
+  body('name').notEmpty(),
+  body('email').isEmail(),
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const {
+      name, phone, email, message
+    } = req.body
+    return controller.addCont(name, phone, email, message)
+      .then((response) => res.status(201).send(response))
+      .catch(({ message: error }) => {
+        res.status(400).send(error)
+      })
+  })
 
 module.exports = router
