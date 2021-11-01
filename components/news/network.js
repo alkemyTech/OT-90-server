fec2d32e206ce3a722873cddcc3f02b81d55e876
verteramo.federico <verteramo.federico@gmail.com>
2021-11-01 14:26:24 -0300

    validate req and middleware admin incorporated

--- components/news/network.js
+++ components/news/network.js
@@ -1,5 +1,7 @@
+const { body, validationResult } = require('express-validator')
 const express = require('express')
 const controller = require('./controller')
+const { isAdmin } = require('../../middleware/index')
 
 const router = express.Router()
 
@@ -11,21 +13,28 @@ router.get('/', async (req, res) => {
     res.status(500).send({ Error: 'Something has gone wrong' })
   }
 })
-router.post('/', async (req, res) => {
-  try {
-    const postNew = await controller.addNew(
-      req.body.name,
-      req.body.content,
-      req.body.image,
-      req.body.categoryId
-    )
-    return res.status(201).send({
-      message: 'New created',
-      postNew
-    })
-  } catch (e) {
-    return res.status(500).send({ error: e })
-  }
-})
+router.post('/', isAdmin,
+  body('name').notEmpty().isLength({ max: 200 }),
+  body('content').notEmpty(),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+      }
+      const postNew = await controller.addNew(
+        req.body.name,
+        req.body.content,
+        req.body.image,
+        req.body.categoryId
+      )
+      return res.status(201).send({
+        message: 'New created',
+        postNew
+      })
+    } catch (error) {
+      return res.status(500).send({ error })
+    }
+  })
 
 module.exports = router
