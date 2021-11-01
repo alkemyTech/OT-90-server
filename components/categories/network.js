048520fc7a21b37c592b02ff807b696310481b9e
Marcelo Nieva <marcelonieva@gmail.com>
2021-11-01 17:07:22 -0300

    add deleteCategory to  category network

--- components/categories/network.js
+++ components/categories/network.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const controller = require('./controller')
+const { isAdmin } = require('../../middleware')
 
 const router = express.Router()
 
@@ -7,6 +8,20 @@ router.get('/', async (req, res) => {
   try {
     const categories = await controller.getAll()
     res.status(200).send(categories)
+  } catch (Error) {
+    res.status(500).send({ Error: 'Something has gone wrong' })
+  }
+})
+
+router.delete('/:id', isAdmin, async (req, res) => {
+  const { params: { id } } = req
+  try {
+    const deleted = await controller.deleteCategory(id)
+    if (!deleted) {
+      res.status(404).send({ Error: `Not founded a category with id ${id}` })
+      return
+    }
+    res.status(204).send()
   } catch (Error) {
     res.status(500).send({ Error: 'Something has gone wrong' })
   }
