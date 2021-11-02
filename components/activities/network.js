6a0f87a3d2deae931f87e84781b77f91bf4e55bd
Maximiliano Paci <maximiliano.paci@sarsy-srl.com>
2021-11-02 10:13:44 -0300

    adding validation schema

--- components/activities/network.js
+++ components/activities/network.js
@@ -1,23 +1,16 @@
-const { body, validationResult } = require('express-validator')
 const express = require('express')
 const controller = require('./controller')
-const { isAdmin } = require('../../middleware/index')
+const { isAdmin, validation } = require('../../middleware/index')
+const { categoryPostSchema } = require('./schema')
 
 const router = express.Router()
 
 router.post(
   '/',
   isAdmin,
-  body('name').notEmpty(),
-  body('content').notEmpty(),
-  body('name').isLength({ max: 200 }),
-  body('content').isLength({ max: 255 }),
+  validation(categoryPostSchema),
   async (req, res) => {
     try {
-      const errors = validationResult(req)
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
-      }
       const activity = await controller.addActivity(req.body.name, req.body.content)
       return res.status(201).send({
         message: 'Activity succesfully created',
