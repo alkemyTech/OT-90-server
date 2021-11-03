130622d7f1bb730eb4b117ed276ba4d580df3fbe
andresmonsa <monsalbefotografia@gmail.com>
2021-11-03 09:55:12 -0300

    change response code

--- components/news/network.js
+++ components/news/network.js
@@ -46,8 +46,8 @@ router.post('/', isAdmin,
 
 router.delete('/:id', isAdmin, async (req, res) => {
   try {
-    await controller.deleteNew(req.params.id)
-    res.status(204).json()
+    response.body = await controller.deleteNew(req.params.id)
+    res.status(200).json(response)
   } catch (e) {
     res.status(400).json(e)
   }
