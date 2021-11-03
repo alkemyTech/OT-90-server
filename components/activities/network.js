2713561959c43dd23fd94fd958f5272642d712d9
Maximiliano Paci <pacimaximiliano@gmail.com>
2021-11-03 12:01:27 -0300

    adding activities put endpoint

--- components/activities/network.js
+++ components/activities/network.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const controller = require('./controller')
 const { isAdmin, validation } = require('../../middleware/index')
-const { activityPostSchema } = require('../../validate/activitySchema')
+const { activityPostSchema, activityPutSchema } = require('../../validate/activitySchema')
 
 const router = express.Router()
 
@@ -13,6 +13,20 @@ router.post(
     try {
       const response = await controller.addActivity(req.body.name, req.body.content)
       return res.status(201).json(response)
+    } catch (failedResponse) {
+      return res.status(500).json(failedResponse)
+    }
+  }
+)
+
+router.put(
+  '/:id',
+  isAdmin,
+  validation(activityPutSchema),
+  async (req, res) => {
+    try {
+      const { id } = req.params
+      return res.status(200).json(id)
     } catch (failedResponse) {
       return res.status(500).json(failedResponse)
     }
