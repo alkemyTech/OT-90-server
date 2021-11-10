c30b2bd0ec06d96c398972002e1719eb82d0c75e
jpuery71292 <66072856+jpuery71292@users.noreply.github.com>
2021-11-10 09:12:05 -0300

    Update controller.js

--- components/activities/controller.js
+++ components/activities/controller.js
@@ -10,7 +10,7 @@ const getAll = async () => {
         {
           id: activity.id,
           name: activity.name,
-          conent: activity.content,
+          content: activity.content,
           image: activity.image
         }
       ))
