7932c6ceb8782f78f9456eebbecb9645eae67f57
Dario Velez Roman <dvelezroman@gmail.com>
2021-11-13 20:16:19 -0300

    refactor deploy and CI scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -8,8 +8,19 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" #loads nvm bash_completion (node is in)
 
+base64 --decode env_encrypted.txt > .env
+
+# Execute docker container for mysql
+docker-compose up -d
+echo "waiting for DOCKER MYSQL CONTAINER..."
+
+while ! docker-compose exec rabbitmq /is_ready.sh; do sleep 1; done
+
+echo "Installing npm modeules ..."
+
 # install node modules
 npm install
 
+echo "Running server ..."
 # run the application
 npm run prod
