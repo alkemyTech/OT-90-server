28c0e82cb66c09fcaadbf222b755fdef89f208c8
Dario Velez Roman <dvelezroman@gmail.com>
2021-11-14 02:29:47 -0300

    fix

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,9 +4,9 @@ sudo chmod -R 777 /home/ubuntu/g90-api
 cd /home/ubuntu/g90-api
 
 # add npm and node to path
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" #loads nvm bash_completion (node is in)
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" #loads nvm bash_completion (node is in)
 
 base64 --decode env_encrypted.txt > .env
 
