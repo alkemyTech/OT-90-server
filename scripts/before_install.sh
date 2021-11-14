4a66b493ce3f21d4841b24e1391ebce7c9b29bf0
Dario Velez Roman <dvelezroman@gmail.com>
2021-11-14 03:49:54 -0300

    fix scripts and export

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# add npm and node to path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" #loads nvm bash_completion (node is in)
+
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v14.18.1/bin/pm2
+
 # download node and npm
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 # . ~/.nvm/nvm.sh
 # install node v14
 # nvm install v14
-nvm use 14
 
 # create our working directory if it does not exist
 DIR="/home/ubuntu/g90-api-2"
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,11 +3,6 @@ sudo chmod -R 777 /home/ubuntu/g90-api-2
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/g90-api-2
 
-# add npm and node to path
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" #loads nvm bash_completion (node is in)
-
 base64 --decode env_encrypted.txt > .env
 
 # Execute docker container for mysql
