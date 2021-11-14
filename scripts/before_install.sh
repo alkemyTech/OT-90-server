a406a9af077932dd5a741b641f8c05bd35a0c6b1
Dario Velez Roman <dvelezroman@gmail.com>
2021-11-14 02:23:21 -0300

    fix

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-. ~/.nvm/nvm.sh
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+# . ~/.nvm/nvm.sh
 # install node v14
-nvm install v14
+# nvm install v14
 
 # create our working directory if it does not exist
 DIR="/home/ubuntu/g90-api"
