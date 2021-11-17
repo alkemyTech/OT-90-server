7b7e80d9d6b67791be8c8cd932b32bd234738ef5
Dario Velez Roman <dvelezroman@gmail.com>
2021-11-17 00:14:29 -0300

    add decrypt command for .env file

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,6 +6,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
+# decrypt .env file
+base64 --decode env_encrypted.txt > .env
 # install node modules
 npm install
 npm run prod
