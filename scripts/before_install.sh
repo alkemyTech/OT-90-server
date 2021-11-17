38d96b6ed792d5db007153ff849f7efdbf4ad586
Dario Velez Roman <dvelezroman@gmail.com>
2021-11-17 00:04:00 -0300

    add some scripts for CI/CD

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
-export HOME="/home/ubuntu/"
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # download node and npm
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-# . ~/.nvm/nvm.sh
-# install node v14
-# nvm install v14
+nvm use 14
 
 # create our working directory if it does not exist
-DIR="/home/ubuntu/g90-api-2"
-if [ -d "$DIR" ]
-then
-  echo "${DIR} exists"
+DIR="/home/ubuntu/g90-api"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists."
 else
-  echo "Creating ${DIR} directory"
+  echo "Creating ${DIR} directory..."
   mkdir ${DIR}
 fi
-
-# apply migrations
-# npx sequelize-cli db:migrate
-# apply seeds
-# npx sequelize-cli db:seed:all
