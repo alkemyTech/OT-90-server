53e61e7361d503c86de0dd7a24ae524afab2bdfa
andresmonsa <monsalbefotografia@gmail.com>
2021-11-17 20:19:58 -0300

    utils implemented

--- utils/createUser.js
+++ utils/createUser.js
@@ -8,9 +8,9 @@ const createTestUser = async (data) => {
     const isUser = await User.findOne({ where: { email } })
     if (isUser !== null) userData.email += 'Alkemy'
     const token = jwt.sign(userData, process.env.JWT_SECRET_KEY)
-      const createdUser = await User.create(userData)
-      createdUser.token = token
-      return createdUser
+    const createdUser = await User.create(userData)
+    createdUser.token = token
+    return createdUser
   } catch (e) {
     return e
   }
