bf40cf8a8a3b1a57cf659276063e98dbf1198a95
verteramofederico <verteramo.federico@gmail.com>
2021-11-17 17:08:14 -0300

    test added to news endpoints

---
+++ test/news.test.js
@@ -0,0 +1,53 @@
+const app = require('../app')
+const request = require("supertest")
+const expect = require("chai").expect
+
+let id 
+
+describe("POST /news", function () {
+  const dataPost = {
+    name: 'testing news',
+    content: 'this a test content',
+    categoryId: 2,
+}
+  it("post news", async function () {
+    const response = await request(app).post("/news").send(dataPost)
+    id = response.body.body.id
+    expect(response.status).to.eql(201)
+  })
+})
+
+describe("GET /news", function () {
+  it("returns all news", async function () {
+    const response = await request(app).get("/news")
+    expect(response.status).to.eql(200)
+  })
+})
+
+describe("GET /news:id", function () {
+  it("returns the one new", async function () {
+    const response = await request(app).get(`/news/${id}`)
+    expect(response.status).to.eql(200)
+  })
+})
+
+describe("PUT /news", function () {
+  const dataPut = {
+    name: 'modifing testing news',
+    content: 'modifing this a test content',
+    categoryId: 1,
+}
+  it("put news", async function () {
+    const response = await request(app).put(`/news/${id}`).send(dataPut)
+    expect(response.status).to.eql(201)
+  })
+})
+
+describe("delete /news", function () {
+  it("delete news", async function () {
+    const response = await request(app).delete(`/news/${id}`)
+    expect(response.status).to.eql(204)
+  })
+})
+
+
