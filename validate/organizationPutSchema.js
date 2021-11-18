17c916f0e046a00544ada918c6c93bb27a632841
Marcelo Nieva <marcelonieva@gmail.com>
2021-11-18 19:47:18 -0300

    update schema

--- validate/organizationPutSchema.js
+++ validate/organizationPutSchema.js
@@ -3,9 +3,9 @@ const yup = require('yup')
 const organizationPutSchema = yup.object({
   body: yup.object({
     name: yup
-      .string().max(200),
+      .string().max(200).required(),
     image: yup
-      .string()
+      .string().required()
   })
 })
 
