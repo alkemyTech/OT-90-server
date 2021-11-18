aa320786a462abb23b0a2801ad766fd068f6009d
jpuery71292 <jpuery@gmail.com>
2021-11-18 11:12:38 -0300

    Create activity-test y endpoint delete

---
+++ test/Activities.test.js
@@ -0,0 +1,91 @@
+const CreateUser = require('../utils/createUser')
+const DeleteUser = require('../utils/hardDeleteUser')
+const request = require('supertest')
+const server = require('../app')
+
+
+describe('hooks', () => {
+  const userData = {
+    firstName: 'alkemy',
+    lastName: 'bootcamp',
+    email: 'usuario@testing27.com',
+    password: 'alkemy123',
+    role: 'Admin'
+  }
+
+  let token
+  let idUser
+  let idActivity
+  before(async () => {
+    try {
+      const user = await CreateUser.createTestUser(userData)
+      console.log(user)
+      token = user.token
+      idUser = user.dataValues.id
+      console.log(id)
+    } catch (e) {
+      console.log(idUser)
+    }
+  })
+
+  after(async () => {
+    try {
+      await DeleteUser.hardDelete('User', idUser)
+      console.log(idUser, 'eliminado')
+    } catch (e) {
+      console.log(e)
+    }
+  })
+
+  // test cases
+  describe('Activities API', () => {
+  /* get / */
+    describe('GET /activities', () => {
+      it('It should GET all the activities', (done) => {
+        request(server)
+          .get('/activities')
+          .set('Acept', 'application/json')
+          .expect(200, done)
+      })
+    })
+
+    /* get:id */
+    describe('GET /activities/id', () => {
+      it('It shoul GET a activity by id', (done) => {
+        request(server)
+          .get('/activities/1')
+          .set('Acept', 'application/json')
+          .expect(200, done)
+      })
+    })
+
+    /* post */
+    describe('POST /activities', () => {
+      it('It shoul POST a new activity', (done) => {
+        const data = { name: 'new Activity', content: 'Send New Activity' }
+        console.log(token)
+        request(server)
+          .post('/activities')
+          .send(data)
+          .set({ authorization: token })
+          .expect(201)
+          .end((err, res) => {
+            if (err) done(err)
+            idActivity = res.body.body.id
+            done()
+          })
+      })
+    })
+
+    describe('PUT /activities', () => {
+      it('It shoul PUT delete activity by id', (done) => {
+        const data = { name: 'Prueba', content: 'Prueba del cambio correcto' }
+        request(server)
+          .put(`/activities/${idActivity}`)
+          .send(data)
+          .set({ authorization: token })
+          .expect(200, done)
+      })
+    })
+  })
+})
