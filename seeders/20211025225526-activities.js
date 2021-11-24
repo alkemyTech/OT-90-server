388c2ae3448d4a19967f21b43f643edd5f94db32
Dario Velez Roman <dvelezroman@gmail.com>
2021-11-24 18:16:02 -0300

    add image to seeder

--- seeders/20211025225526-activities.js
+++ seeders/20211025225526-activities.js
@@ -5,16 +5,19 @@ module.exports = {
           {
             name: 'Beca estímulo',
             content: 'los jóvenes reciben una beca estímulo que es supervisada por los tutores).',
+            image: 'https://centromedicomontemar.cl/wp-content/uploads/2015/06/sin-perfil.png',
             createdAt: new Date(),
             updatedAt: new Date()
           },{
             name: 'Acompañamiento escolar y familiar:',
             content: 'Los tutores son encargados de articular con la familia y con las escuelas de los jóvenes para monitorear el estado de los tutoreados)',
+            image: 'https://centromedicomontemar.cl/wp-content/uploads/2015/06/sin-perfil.png',
             createdAt: new Date(),
             updatedAt: new Date()
           },{
             name: 'Ayudantías',
             content: '(los que comienzan el 2do años del programa deben elegir una de las actividades que se realizan en la institución para acompañarla e ir conociendo como es el mundo laboral que les espera).',
+            image: 'https://centromedicomontemar.cl/wp-content/uploads/2015/06/sin-perfil.png',
             createdAt: new Date(),
             updatedAt: new Date()
           }          
